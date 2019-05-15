1e19ea1443a14882b5cd4721c0d9354872c0ec92
Yggdrasilxd <SDn8i7cXeQh3Kni>
2019-05-15 12:28:32 +0200

    Erreur

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
   def premier
-    render html : "Bonjour tout le monde on va y arriver ... enfin peut etre"
+    render html: "Bonjour tout le monde on va y arriver ... enfin peut etre"
   end
 end
